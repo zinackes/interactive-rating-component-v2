afc4791a220bc7d5063f3c29bee7097223acf038
zinacke <88772783+zinacke@users.noreply.github.com>
2022-12-17 12:03:24 +0100

    ggg

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
-const note = document.querySelectorAll("button");
+var submit = document.getElementById("submit");
 var mode = document.getElementById("switch");
 var sun = document.querySelector(".fa-sun");
 var error = document.querySelector(".error");
+var txt_mode = document.querySelector("#txt-mode");
+var img_mode = document.querySelector("#img-mode");
+var values = document.querySelectorAll("#star");
+var text = document.getElementById("note");
+var main = document.getElementById("main");
+var second = document.getElementById("second");
 
-mode.onclick = function(txt){
+mode.onclick = function(txt_mode){
     document.body.classList.toggle("light-mode");
 
-    sun.classList.toggle("fa-moon");
-    txt.target.textContent == "Light Mode" ?
-    txt.target.textContent = "Dark Mode" : 
-    txt.target.textContent = "Light Mode";
+    if (txt_mode.target.textContent == "Light Mode"){
+        img_mode.src = "./images/dark-mode.png";
+    } else {
+        img_mode.src = "./images/light-mode.png";
+    }
+    
+    txt_mode.target.textContent == "Light Mode" ?
+    txt_mode.target.textContent = "Dark Mode" : 
+    txt_mode.target.textContent = "Light Mode";
+}
+
+values.forEach((value) => {
+    value.addEventListener("click", () => {
+        text.innerHTML = value.innerHTML;
+
+        value.style.backgroundColor = "var(--bg-round-selected-x)";
+        value.style.color = "white";
+        
+        submit.onclick = function(){
+            main.classList.add("hidden");
+            second.classList.remove("hidden");
+        }
+    })
+})
+
+submit.onclick = function(){
+    error.classList.remove("hidden");
 }
 

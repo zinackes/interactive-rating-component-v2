f9f5ad1f1a88f8f80352e7d7e06c01066d806696
zinacke <88772783+zinacke@users.noreply.github.com>
2022-12-18 00:16:46 +0100

    fvbgbf

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ var values = document.querySelectorAll("#star");
 var text = document.getElementById("note");
 var main = document.getElementById("main");
 var second = document.getElementById("second");
+var active = document.activeElement;
+
 
 mode.onclick = function(txt_mode){
+
     document.body.classList.toggle("light-mode");
 
     if (txt_mode.target.textContent == "Light Mode"){
@@ -27,12 +30,24 @@ values.forEach((value) => {
     value.addEventListener("click", () => {
         text.innerHTML = value.innerHTML;
 
-        value.style.backgroundColor = "var(--bg-round-selected-x)";
-        value.style.color = "white";
-        
         submit.onclick = function(){
             main.classList.add("hidden");
             second.classList.remove("hidden");
+        }
+        mode.onclick = function(txt_mode){
+
+            document.body.classList.toggle("light-mode");
+        
+            if (txt_mode.target.textContent == "Light Mode"){
+                img_mode.src = "./images/dark-mode.png";
+            } else {
+                img_mode.src = "./images/light-mode.png";
+            }
+            
+            txt_mode.target.textContent == "Light Mode" ?
+            txt_mode.target.textContent = "Dark Mode" : 
+            txt_mode.target.textContent = "Light Mode";
+            value.focus();
         }
     })
 })

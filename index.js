b8cdeeb5f7e37319375ccd0d1ba5456a8d1f6c50
zinacke <88772783+zinacke@users.noreply.github.com>
2022-12-21 13:35:00 +0100

    gfhfggf

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var secondvalues = document.querySelectorAll("#second-star");
 var one = document.getElementById("one");
 var two = document.querySelectorAll("#two");
 var back = document.getElementById("back");
-var first_note = document.getElementById("second-star");
+var note_selected = document.querySelector("#two > #star:first-child");
 
 
 mode.onclick = function(txt_mode){
@@ -33,67 +33,60 @@ mode.onclick = function(txt_mode){
     txt_mode.target.textContent = "Light Mode";
 }
 
-values.forEach((value, final_value) => {
+values.forEach((value) => {
     value.addEventListener("click", () => {
-        for (let i = 0; i<4; i++){
-            text[i].innerHTML = value.innerHTML;
+        
+        if (back.classList.contains("hidden")){
+            for (let i = 0; i<4; i++){
+                text[i].innerHTML = value.innerHTML;
+            }
         }
 
+        note_selected.focus();
+
+        if (value < 5){
+            one.classList.add("hidden");
+            back.classList.remove("hidden");
+            for (let i =0; i<2; i++){
+                two[i].classList.remove("hidden");
+            }
+            console.log("marche")
+        }
+        else{
+            console.log("marche pas")
+            console.log(value);
+        }
 
 
         submit.onclick = function(){
             main.classList.add("hidden");
             second.classList.remove("hidden");
         }
-
-
-        one.classList.add("hidden");
-        back.classList.remove("hidden");
-        for (let i =0; i<2; i++){
-            two[i].classList.remove("hidden");
-        }
-        first_note.focus();
-        near_final.innerHTML = value.innerHTML;
-
-        final_value.addEventListener("click", () => {
+            
+        mode.onclick = function(txt_mode){
+    
+            document.body.classList.toggle("light-mode");
+            
+            if (txt_mode.target.textContent == "Light Mode"){
+                img_mode.src = "./images/dark-mode.png";
+            } else {
+                img_mode.src = "./images/light-mode.png";
+            }            
+            txt_mode.target.textContent == "Light Mode" ?
+            txt_mode.target.textContent = "Dark Mode" : 
+            txt_mode.target.textContent = "Light Mode";
+            value.focus();
+            }
+            
             final.innerHTML = value.innerHTML;
-    
-    
-            submit.onclick = function(){
-                main.classList.add("hidden");
-                second.classList.remove("hidden");
-            }
-            
-            mode.onclick = function(txt_mode){
-    
-                document.body.classList.toggle("light-mode");
-            
-                if (txt_mode.target.textContent == "Light Mode"){
-                    img_mode.src = "./images/dark-mode.png";
-                } else {
-                    img_mode.src = "./images/light-mode.png";
-                }
-                
-                txt_mode.target.textContent == "Light Mode" ?
-                txt_mode.target.textContent = "Dark Mode" : 
-                txt_mode.target.textContent = "Light Mode";
-                value.focus();
-            }
         })
     })
-})
 
-
-
-secondvalues.forEach((value) => {
-    value.addEventListener("click", () => {
-    })
-})
 
 back.onclick = function(){
     one.classList.remove("hidden");
     back.classList.add("hidden");
-    for (let i =0; i<3; i++){
+    for (let i =0; i<2; i++){
         two[i].classList.add("hidden");
     }
 }
